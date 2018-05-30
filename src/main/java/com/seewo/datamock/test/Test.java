5189e84509de0ce401cf6b03fa736060be28a480
gaoxinyu <853108389@qq.com>
2018-05-30 09:17:54 +0800

    添加拦截器

---
+++ src/main/java/com/seewo/datamock/test/Test.java
@@ -0,0 +1,24 @@
+package com.seewo.datamock.test;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @Author NianGao
+ * @Date 2018/5/29.
+ * @description
+ */
+public class Test implements ResponseBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+        return false;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return null;
+    }
+}
